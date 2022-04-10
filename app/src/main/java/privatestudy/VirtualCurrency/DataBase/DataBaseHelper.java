69225bbc30e142da49e9b85098974d90d3070637
Shunya Yokoi <88517400+shun-1129@users.noreply.github.com>
2022-04-10 23:14:44 +0900

    commit

--- app/src/main/java/privatestudy/VirtualCurrency/DataBase/DataBaseHelper.java
+++ app/src/main/java/privatestudy/VirtualCurrency/DataBase/DataBaseHelper.java
@@ -30,6 +30,32 @@ public class DataBaseHelper extends SQLiteOpenHelper {
             DBEntry2.COLUMN_NAME_INPUT + " INTEGER," +
             DBEntry2.COLUMN_NAME_INTEREST + " REAL);"
         );
+
+        DB.execSQL(
+            "CREATE TABLE " + DBEntry3.TABLE_NAME + "(" +
+            DBEntry3.COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
+            DBEntry3.COLUMN_NAME_YEAR + " INTEGER," +
+            DBEntry3.COLUMN_NAME_MONTH + " INTEGER," +
+            DBEntry3.COLUMN_NAME_DAY + " INTEGER," +
+            DBEntry3.COLUMN_NAME_RESULT + " REAL);"
+        );
+
+        DB.execSQL(
+            "INSERT INTO " + DBEntry1.TABLE_NAME + "(" +
+            DBEntry1.COLUMN_NAME_BNB + "," +
+            DBEntry1.COLUMN_NAME_CAKE + "," +
+            DBEntry1.COLUMN_NAME_COMMISSION1  + "," +
+            DBEntry1.COLUMN_NAME_COMMISSION2 + ") VALUES (" +
+            "53000.0, 1000.0, 0.0005, 0.0005);"
+        );
+
+        DB.execSQL(
+            "INSERT INTO " + DBEntry2.TABLE_NAME + "(" +
+            DBEntry2.COLUMN_NAME_INITIAL + "," +
+            DBEntry2.COLUMN_NAME_INPUT + "," +
+            DBEntry2.COLUMN_NAME_INTEREST + ") VALUES (" +
+            "3.5118, 10000.0, 0.4933)"
+        );
     }
 
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
